4bfe7a0a36638560c384fa89e806436fd1dd68cf
Adnan Ahmed <adnanahmed.indian@gmail.com>
2022-02-04 00:32:05 +0530

    Alignment of Chess players

--- Frontend/src/gameLogic/Board.js
+++ Frontend/src/gameLogic/Board.js
@@ -2,13 +2,14 @@ import React from "react";
 
 export default function Board({ board }) {
   return (
-    <div>
-        {
-            board.flat().map((piece,i)=> {
-                console.log(piece)
-                return <div key={i}>{JSON.stringify(piece)}</div>
-            })
-        }
+    <div className="board">
+      {board.flat().map((piece, i) => {
+        return (
+          <div key={i} className="square">
+            {JSON.stringify(piece)}
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
